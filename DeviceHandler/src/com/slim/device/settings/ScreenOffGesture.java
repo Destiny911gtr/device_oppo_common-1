c9a47e7fc70e3118b884ade55e721f3726c1706f
Ded_Boi <dhanush911gtr@gmail.com>
2018-04-16 13:04:58 +0000

    common: util: gzosp > aim
    Signed-off-by: Ded_Boi <dhanush911gtr@gmail.com>

--- DeviceHandler/src/com/slim/device/settings/ScreenOffGesture.java
+++ DeviceHandler/src/com/slim/device/settings/ScreenOffGesture.java
@@ -37,10 +37,10 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
-import com.android.internal.util.gzosp.AppHelper;
-import com.android.internal.util.gzosp.ActionConstants;
-import com.android.internal.util.gzosp.DeviceUtils;
-import com.android.internal.util.gzosp.DeviceUtils.FilteredDeviceFeaturesArray;
+import com.android.internal.util.aim.AppHelper;
+import com.android.internal.util.aim.ActionConstants;
+import com.android.internal.util.aim.DeviceUtils;
+import com.android.internal.util.aim.DeviceUtils.FilteredDeviceFeaturesArray;
 
 import com.slim.device.KernelControl;
 import com.slim.device.R;
